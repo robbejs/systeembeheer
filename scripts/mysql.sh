57d0470fe4ca0831365d9175f1bf68758d3e81ed
root <root@robbe-jacobs1.uclllabs.be>
2022-11-14 16:14:54 +0100

    cleanup

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+
 SQL="SELECT count(*) FROM check.log"
 count=$(mysql -u root -p"RootPassword258" -s -N  -e "$SQL")
-
-if (( $count > 90 )); then
+echo $count
+if [ "$count" -gt "90"  ]; then
 	limit=$((count - 90));
 	SQL1="DELETE FROM check.log ORDER BY date ASC limit $limit"
 	mysql -u root -p"RootPassword258" -s -N  -e "$SQL1"
